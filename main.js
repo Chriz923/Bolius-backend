f86f68f16a07ecb4ff0a0459e977e0712343251f
chri923t <chri923t@stud.kea.dk>
2022-12-07 11:49:55 +0100

    Backend

---
+++ main.js
@@ -0,0 +1,158 @@
+let Boliusexampels = [
+    {
+        id: 1,
+        region: "Hovedstaden",
+        primary:
+        {
+         location:"Søbredden 27A 2820 Gentofte",
+         livingspace: 129,
+         price: 4644000,
+         energylabel: "A",
+         investment:
+             {
+               hollowwallisolation:14209,
+               solarcells:110416,
+               windowisolation: ,
+             }
+        },
+        secondary:
+        {
+        location: "Spicas Alle 26 2770 Kastrup",
+        livingspace: 132,
+        price:4995000,
+        energylabel: "D",
+        investment:
+            {
+              hollowwallisolation:14479,
+              solarcells:110416,
+              windowisolation: ,
+
+            }
+        },
+
+    },
+    {
+        id: 2,
+        region:"Zealand",
+        primary:
+            {
+             location:"Sydkærsvej 99",
+             livingspace: 181,
+             price: 7695000,
+             energylabel: "A",
+             investment:
+                 {
+                  hollowwallisolation:18791,
+                  solarcells:110416,
+                  windowisolation: ,
+                 }
+            },
+        secondary:
+            {
+             location:"Niels Juelsgade 3 4600 Køge",
+             livingspace: 93,
+             price:3295000,
+             energylabel: "D",
+             investment:
+                {
+                    hollowwallisolation:11110,
+                    solarcells:110416,
+                    windowisolation: ,
+                },
+            },
+
+    },
+    {
+     id: 3,
+     region:"NorthJutland",
+     primary:
+         {
+          location:"Pilevej 5 9990 Skagen",
+          livingspace: 150,
+          price: 2345000,
+          energylabel: "B",
+          investment:
+              {
+                  hollowwallisolation: 16096,
+                  solarcells: 110416,
+                  windowisolation: ,
+              }
+         },
+        secondary:
+            {
+            location:"Fredensklit 8 9990 Skagen",
+            livingspace: 146,
+            price: 6000000,
+            energylabel: "E",
+            investment:
+                {
+                 hollowwallisolation: 15691,
+                 solarcells: 110416,
+                 windowisolation: ,
+                }
+            },
+
+    },
+    {
+     id:4,
+     region: "MiddleJutland",
+     primary:
+         {
+          location:"Valdemars Have 59 Voel, 8600 Silkeborg",
+          livingspace: 140,
+          price:2793000,
+          energylabel: "A",
+          investment:
+              {
+                  hollowwallisolation: 15287,
+                  solarcells: 110416,
+                  windowisolation: ,
+              }
+         },
+        secondary:
+            {
+             location: "Nybovej 22 Nybo, 7400 Herning",
+             livingspace: 166,
+             price: 3095000,
+             energylabel: "D",
+             investment:
+                 {
+                     hollowwallisolation: 17443,
+                     solarcells: 110416,
+                     windowisolation: ,
+                 }
+            }
+    },
+    {
+     id:5,
+     region:"SouthDenmark",
+     primary:
+         {
+          location: "Christiansfeldvej 34 6100 Haderslev",
+          livingspace: 197,
+          price: 4395000,
+          energylabel: "B",
+          investment:
+              {
+                  isolation: 20273,
+                  solarcells: 110416,
+                  windowisolation: ,
+              }
+         },
+        secondary:
+            {
+             location: "Erlevvej 65 6100 Haderslev",
+             livingspace: 148,
+             price: 2495000,
+             energylabel: "D",
+             investment:
+                 {
+                     isolation: 15961,
+                     solarcells: 110416,
+                     windowisolation: ,
+                 }
+            }
+    }
+
+];
+module.exports = Boliusexampels
